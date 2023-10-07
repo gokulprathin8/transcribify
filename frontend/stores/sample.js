0cfe606a425c1c5b12d54c4da99527708ef7338a
Gokul Prathin <gokulprathin8@gmail.com>
2023-10-07 17:20:54 -0400

    added zustand

---
+++ frontend/stores/sample.js
@@ -0,0 +1,8 @@
+import create from 'zustand';
+
+const useMyStore = create((set) => ({
+    count: 0,
+    increment: () => set((state) => ({ count: state.count + 1 })),
+}));
+
+export default useMyStore;
