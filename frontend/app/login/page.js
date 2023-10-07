f496c11fa2bfdf7dcd08e056a8350a7936407257
Gokul Prathin <gokulprathin8@gmail.com>
2023-10-07 15:40:39 -0400

    added login page structure

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -1,9 +1,51 @@
-import React from "react";
+"use client"
+
+import React from 'react';
+import styled from 'styled-components';
+import Image from 'next/image';
+
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+`;
+
+const ImageContainer = styled.div`
+  position: relative;
+  width: 30%;
+  height: 100%;
+`;
+
+const CodeBlockContainer = styled.div`
+  flex: 1;
+`;
 
 export default function LoginPage() {
     return (
-        <div>
-            test
-        </div>
-    )
+        <LoginContainer>
+            <ImageContainer>
+                <Image
+                    src="/static/login-page.jpg"
+                    alt="Image Description"
+                    layout="fill"
+                    objectFit="cover"
+                    objectPosition="center"
+                />
+            </ImageContainer>
+
+            <CodeBlockContainer>
+                <form>
+                    <label>
+                        Username:
+                        <input type="text" name="username" />
+                    </label>
+                    <label>
+                        Password:
+                        <input type="password" name="password" />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+            </CodeBlockContainer>
+        </LoginContainer>
+    );
 }
