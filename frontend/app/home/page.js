37316d6a65a1a6afb769892fef55e8e2e0c70f33
somethingdevs <somethingdevs@gmail.com>
2023-10-08 02:23:52 -0400

    refactor: Zustand revert

---
+++ frontend/app/home/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import styled from "styled-components";
+import { Button, Input, Upload, Avatar, Icon } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+
+function DashboardPage() {
+    
+}
+
+export default DashboardPage;
